bebace190c16aedc56c84607afecbb49964dd029
Mike Rogers <me@mikerogers.io>
2020-12-04 15:03:21 +0000

    Changing approach based upon request type

--- app/javascript/service_workers/service-worker.js
+++ app/javascript/service_workers/service-worker.js
@@ -1,11 +1,33 @@
-console.log('Hey I am IN the server Worker');
+import { registerRoute } from 'workbox-routing';
+import { NetworkFirst, StaleWhileRevalidate, CacheFirst } from 'workbox-strategies';
 
-import {registerRoute} from 'workbox-routing';
-import {CacheFirst} from 'workbox-strategies';
+// Used for filtering matches based on status code, header, or both
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
+// Used to limit entries in cache, remove entries after a certain period of time
+import { ExpirationPlugin } from 'workbox-expiration';
 
+// Loading pages, checks the network first
 registerRoute(
-  ({request}) => true,
+  ({request}) => (console.log(request) && false) ||
+  request.destination === "document",
+  new NetworkFirst({
+    cacheName: 'documents',
+  })
+);
+
+// For assets, we check the cache first
+registerRoute(
+  ({request}) => request.destination === "script" ||
+  request.destination === "style",
   new CacheFirst({
-    cacheName: 'static-resources',
+    cacheName: 'assets-styles-and-scripts',
+  })
+);
+
+// For assets, we check the cache first
+registerRoute(
+  ({request}) => request.destination === "image",
+  new CacheFirst({
+    cacheName: 'assets-images',
   })
 );
